1501f8413b83c3ea3c1b35af826200225540b027
enjoeyland <khmin1104@gmail.com>
2022-11-11 01:59:51 +0900

    fix(lex): interpret even if token end with EOF.

--- main.c
+++ main.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 
+#include "execute.h"
 #include "lex_analyzer.h"
 #include "parser.h"
 #include "token_type.h"
 
 extern TokenParser tp;
 void interpret(Token* token_statement, int len) {
-    printStringTable();
-    printSymbolTable();
-
-    for (int i = 0; i < len; i++) {
-        printf("%d ", token_statement[i].type);
-    }
-    printf("\n");
+    // for (int i = 0; i < len; i++) {
+    //     printf("%d ", token_statement[i].type);
+    // }
+    // printf("\n");
 
     tp = (TokenParser){token_statement, len, 0};
     ParseNode* syntax_tree = statement();
-    printf("done make syntax tree\n");
-    print_syntax_tree(syntax_tree);
-    printf("done print syntax tree\n");
+
+    // printf("done make syntax tree\n");
+    // print_syntax_tree(syntax_tree);
+    // printf("done print syntax tree\n");
+
+    ParseNode* result = execute(syntax_tree);
+    print_token(result->current);
+    printf("\n");
+
+    // printStringTable();
+    // printSymbolTable();
 }
 
 int main() {
     Token statement[100];
     int len = 0;
     Token token;
+    printf("> ");
     while (!isFailToken(token = getNextToken())) {
         statement[len++] = token;
         if (isNewlineToken(token)) {
             interpret(statement, len);
             len = 0;
+            printf("> ");
         }
     }
+    statement[len++] = (Token){NEW_LINE, 0, NONE};
+    interpret(statement, len);
 }
